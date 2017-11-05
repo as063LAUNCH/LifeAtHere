aec60e63525506e14a63031b0aa211be2dfee3a9
as063LAUNCH <as063@bucknell.edu>
2017-11-05 15:25:00 -0500

    log in button added

---
+++ src/javascript.js
@@ -0,0 +1,55 @@
+var EVENTS_URL = "https://myapi.bucknell.edu/framework/data/communication/event/";
+
+function logIn() {
+	//have javascript to log in now
+	var response = httpGet(EVENTS_URL);
+	console.log(response);
+	console.log("Logging In");
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+
+$('.responsive').slick({
+  dots: true,
+	prevArrow: $('.prev'),
+	nextArrow: $('.next'),
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
