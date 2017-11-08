ec2403a25fd76d651850a2c069bae0e0f05d87dd
Anushikha Sharma <as063@bucknell.edu>
2017-11-08 16:14:11 -0500

    search button

--- src/javascript.js
+++ src/javascript.js
@@ -8,6 +8,14 @@ function logIn() {
 	console.log(response);
 }
 
+function GetEvents() {
+	//have javascript to log in now
+  console.log("GetEvents");
+	var response = httpGet("http://localhost:8888");
+  console.log("Response received");
+	console.log(response);
+}
+
 function httpGet(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
@@ -16,42 +24,7 @@ function httpGet(theUrl)
     return xmlHttp.responseText;
 }
 
-/**
-$('.responsive').slick({
-  dots: true,
-	prevArrow: $('.prev'),
-	nextArrow: $('.next'),
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-*/
+var text = prompt("Filter your events");
+
+
+
