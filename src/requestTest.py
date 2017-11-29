d6e4ad7fde7da309e9617ace4b0dec6164956e17
Lucas Nicolois <pln005@bucknell.edu>
2017-11-29 17:04:21 -0500

    oh boy

--- src/requestTest.py
+++ src/requestTest.py
@@ -48,32 +48,20 @@ EVENTS_URL = "https://myapi.bucknell.edu/framework/data/communication/event/?acc
 
 response = requests.get(EVENTS_URL)
 events = response.json()
-
+'''
 BUILDING_URL = "https://myapi.bucknell.edu/framework/data/building/summary/?access_token=70c746e5-0a28-4c56-a4b7-84ade9d05940"
 response = requests.get(BUILDING_URL)
 buildings = response.json()
-
+'''
 '''
 Adds in missing Longitude and Latitude information so that we can display a location
 This needs more work to add in missing locations, but is a good start
 '''
 
-def findLocation(event):
-    location = (0.0,0.0)
-    loc = event['Locations'][0]['Location']
-    for build in buildings:
-        if(build['Name'] in loc):
-            location = (build['Latitude'],build['Longitude'])
-    '''if location == (0.0,0.0):
-        locs.add(loc)
-    '''
-    event['Latitude'] = location[0]
-    event['Longitude'] = location[1]
-    return event
-
+'''
 for event in events:
     findLocation(event)
-
+'''
 ### Get the unique locations, maybe we can find default images for them. ###
 '''
 locations = list(map((lambda e: e[LOCATIONS][0]["Location"]), events))
